c6f3b00c95eb4e6df1fc12af24b1b70f010018b1
GrisGGG <astropoetscapicua@gmail.com>
2022-03-21 20:30:21 -0600

    agregando detalles

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,16 @@ const fetchPokemon = () => {
             pokeImage(pokeImg);
             let typepokemon = data.types[0].type.name;
             pokeType(typepokemon);
-
+            let heightPokemon = data.height;
+            pokeHeight(heightPokemon);
+            let movesPokemon = data.moves[0].move.name;
+            let sndMove = data.moves[1].move.name;
+            let trdMove = data.moves[2].move.name;
+            pokeMoves(movesPokemon +"," + sndMove + "," + trdMove);
+            let statsPokemon = data.stats[0].stat.name;
+            let base_Stat = data.stats[0].base_stat;
+            let effort = data.stats[0].effort;
+            pokeStats("Name: " + statsPokemon + "\nBase stat: " + base_Stat + "\nEffort: " + effort);
         }
     });
 }
@@ -25,9 +34,20 @@ const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImg");
     pokePhoto.src = url;
 }
-
 const pokeType = (url) =>{
     const typePokemon = document.getElementById("type-pokemon");
-    typePokemon.innerText = ("Tipo: " + url);
+    typePokemon.innerText = ("Type: " + url);
     
+}
+const pokeHeight = (url) =>{
+    const heightPoke = document.getElementById("height-pokemon");
+    heightPoke.innerText = ("Height: " + url);
+}
+const pokeMoves = (url) =>{
+    const movesPoke = document.getElementById("moves-pokemon");
+    movesPoke.innerText = ("Moves \n " + url)
+}
+const pokeStats = (url) =>{
+    const statsPoke = document.getElementById("stats-pokemon");
+    statsPoke.innerText = ("Stats \n" + url);
 }
